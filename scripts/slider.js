8673ab3aea2bce4d469bbb15aaaff03366bd6590
AnnaZiankevich <anna.ziankevich@gmail.com>
2023-08-30 23:20:24 +0300

    Removed needless files, button hides when slider item's clicked

--- scripts/slider.js
+++ scripts/slider.js
@@ -17,7 +17,14 @@ $(".services-block__slider-body").owlCarousel({
   });
   $(document).ready(function () {
     $(".services-block__slider-item").click(function () {
+      let $button = $(this).find('.services-block__slider-button--green');                                                         
       $(".services-block__slider-item").not($(this)).removeClass("active");
+      $button.not($button).removeClass("btn-active");
       $(this).toggleClass("active");
+      $button.toggleClass("btn-active");
     });
   });
+  function HideButton({ value }) {
+    let button = document.getElementById('services-block__slider-button--green');
+    button.hidden = value;
+  };
